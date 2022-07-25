9f773ac257a74587a9f0a3a7a12b8846d1e3ded5
Julia Friesel <julia.friesel@gmail.com>
2022-07-25 14:41:49 +0200

    Add oh-my-zsh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc czrc eslintrc gitconfig gitignore_global tmux.conf vimrc zshrc zsh-update"    # list of files/folders to symlink in homedir
+files="bashrc czrc eslintrc gitconfig gitignore_global oh-my-zsh tmux.conf vimrc zshrc zsh-update"    # list of files/folders to symlink in homedir
 
 ##########
 
